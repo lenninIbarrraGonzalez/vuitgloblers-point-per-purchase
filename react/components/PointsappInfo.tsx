eff6598d53bc9516b9f1f571404c63e4c2acbffc
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-06-22 16:37:09 -0500

    feature/HUID-001-initial-setup: installamos e implementamos proptypes para validar y estandarizar el tipo de propiedades del componente PointappInfo

--- react/components/PointsappInfo.tsx
+++ react/components/PointsappInfo.tsx
@@ -1,23 +1,44 @@
 import React from "react"
+import PropTypes from "prop-types"
 import { useProduct } from "vtex.product-context"
 
+type Props = {
+  basePoint : number,
+  quantityPoint: number
 
-const PointsappInfo = () =>  {
+}
+
+const PointsappInfo = ({basePoint, quantityPoint}: Props) =>  {
   const productContextValue = useProduct();
   let sellingPrice =  productContextValue.product.priceRange.sellingPrice.highPrice
   sellingPrice = parseInt(sellingPrice, 10);
 
-  let basePoint = parseInt("500",10)
-  let quantityPoint = parseInt("2",10)
+  console.log(basePoint,quantityPoint);
+
+  // let basePoint = parseInt("500",10)
+  // let quantityPoint = parseInt("2",10)
 
   let numberPoint = Math.floor((sellingPrice/basePoint))
   numberPoint = numberPoint * quantityPoint
 
   return <>
   {/* <pre>{JSON.stringify(productContextValue, null, 4)}</pre> */}
+  {/* <pre>Te puedes ganar {numberPoint} puntos con esta compra</pre> */}
   <pre>Te puedes ganar {numberPoint} puntos con esta compra</pre>
+  <p>{basePoint}</p>
+  <p>{quantityPoint}</p>
   </>
 
 }
 
+PointsappInfo.propTypes = {
+  basePoint: PropTypes.number.isRequired,
+  quantityPoint: PropTypes.number.isRequired
+}
+
+PointsappInfo.defaultProps = {
+  basePoint: 500,
+  quantityPoint: 1
+
+}
 export default PointsappInfo
