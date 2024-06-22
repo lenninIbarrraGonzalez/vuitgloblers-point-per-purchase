b58fbf3a404c91b76abf15f11a0d43fd618e6ba2
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-06-22 15:20:58 -0500

    feature/HUID-001-initial-setup: se hace uso del hook de useProduct del product context para capturar el precio del articulo y se realizan los calculos con valores fijos, se muestra el resultado en la pagaina de producto

--- react/components/PointsappInfo.tsx
+++ react/components/PointsappInfo.tsx
@@ -1,4 +1,23 @@
 import React from "react"
-const PointsappInfo = () => <h1>puntos a ganar</h1>
+import { useProduct } from "vtex.product-context"
+
+
+const PointsappInfo = () =>  {
+  const productContextValue = useProduct();
+  let sellingPrice =  productContextValue.product.priceRange.sellingPrice.highPrice
+  sellingPrice = parseInt(sellingPrice, 10);
+
+  let basePoint = parseInt("500",10)
+  let quantityPoint = parseInt("2",10)
+
+  let numberPoint = Math.floor((sellingPrice/basePoint))
+  numberPoint = numberPoint * quantityPoint
+
+  return <>
+  {/* <pre>{JSON.stringify(productContextValue, null, 4)}</pre> */}
+  <pre>Te puedes ganar {numberPoint} puntos con esta compra</pre>
+  </>
+
+}
 
 export default PointsappInfo
